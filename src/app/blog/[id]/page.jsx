ee36d12eaaeefb3af0e88d18f549e195d51c62a2
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-10-12 02:23:17 -0700

    commit 5

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,20 +1,28 @@
 import styles from './singlePost.module.css';
 import Image from 'next/image';
-export default function SingleBlogPage(){
-    return(
+
+export default function SingleBlogPage() {
+    return (
         <div className={styles.container}>       
             <div className={styles.imgContainer}>
-                <Image src='/post.jpg' alt="" fill className={styles.img} />
+                <Image
+                    src='/post.jpg'
+                    alt=""
+                    fill /* Allows the image to fill its container */
+                    objectFit="cover" /* Ensures the image covers the container */
+                    className={styles.img}
+                />
             </div>       
             <div className={styles.textContainer}>
                 <h1 className={styles.title}>Title</h1>
                 <div className={styles.detail}>    
                     <Image 
-                          className={styles.avatar}
-                          src='/post2.webp' 
-                          alt='' 
-                          height={50}
-                          width={50} />            
+                        className={styles.avatar}
+                        src='/post2.webp' 
+                        alt='' 
+                        height={50}
+                        width={50}
+                    />            
                     <div className={styles.detailText}>
                         <span className={styles.detailTitle}>Author</span>
                         <span className={styles.detailValue}>Zerihun Solomon</span>
@@ -24,8 +32,10 @@ export default function SingleBlogPage(){
                         <span className={styles.detailValue}>01.01.2024</span>
                     </div>
                 </div>
-                <div className={styles.content}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro doloribus eligendi ipsa, inventore accusantium excepturi vero atque? Fugit commodi, ea excepturi nulla distinctio eveniet, deleniti at perferendis alias porro ad!</div>
+                <div className={styles.content}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro doloribus eligendi ipsa, inventore accusantium excepturi vero atque? Fugit commodi, ea excepturi nulla distinctio eveniet, deleniti at perferendis alias porro ad!
+                </div>
             </div>
-      </div>
+        </div>
     )
 }
