d5e263d6fd9e65763df8c3a968783f6566227152
@zera-sol <zedomanwithjesu1994@gmail.com>
2024-10-23 15:38:45 -0700

    almost last 2

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -6,9 +6,9 @@ export default function PostCard({post}){
       <div className={styles.container}>
             <div className={styles.top}>
                 <div className={styles.imgContainer}>
-                    <Image src='/post.jpg' alt="" fill className={styles.img}/>
+                    <Image src={post.img?post.img:"/post.jpg"} alt="" fill className={styles.img}/>
                 </div>
-                <span className={styles.date}>01.01.2024</span>
+                <span className={styles.date}>{post.createdAt.slice(0, 10).split("-").join(".")}</span>
             </div>
             <div className={styles.bottom}>
                 <h1 className={styles.title}>{post.title}</h1>
